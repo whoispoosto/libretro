c430f538bc8adef0feae158301643b9c7643ccfd
Preston <prestonmeek6@gmail.com>
2025-12-12 23:32:12 -0600

    Draw multicolor triangle

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ unsigned int indices[] = {
     0, 1, 3, // first triangle
     1, 2, 3  // second triangle
 };
+
 const char *vertexShaderSource =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -20,7 +21,7 @@ const char *vertexShaderSource =
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource =
+const char *fragmentShaderSource1 =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
@@ -28,8 +29,17 @@ const char *fragmentShaderSource =
     "  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\0";
 
+const char *fragmentShaderSource2 =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "  FragColor = vec4(0.5f, 0.5f, 0.9f, 1.0f);\n"
+    "}\0";
+
 int main(void) {
-  unsigned int VAO, VBO, EBO, vertexShader, fragmentShader, shaderProgram;
+  unsigned int VAO, VBO, EBO, vertexShader, fragmentShader1, fragmentShader2,
+      shaderProgram1, shaderProgram2;
 
   const auto init_cb = [&]() noexcept {
     glGenVertexArrays(1, &VAO);
@@ -43,7 +53,7 @@ int main(void) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    /* Bind element buffer object to VAO */
+    /* Bind element buffer object to vertex array object (VAO) */
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                  GL_STATIC_DRAW);
@@ -65,35 +75,50 @@ int main(void) {
     glCompileShader(vertexShader);
 
     /* Create fragment shader */
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
+    fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, nullptr);
+    glCompileShader(fragmentShader1);
+
+    fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, nullptr);
+    glCompileShader(fragmentShader2);
 
     /* Link shaders into program */
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    shaderProgram1 = glCreateProgram();
+    glAttachShader(shaderProgram1, vertexShader);
+    glAttachShader(shaderProgram1, fragmentShader1);
+    glLinkProgram(shaderProgram1);
+
+    shaderProgram2 = glCreateProgram();
+    glAttachShader(shaderProgram2, vertexShader);
+    glAttachShader(shaderProgram2, fragmentShader2);
+    glLinkProgram(shaderProgram2);
 
     /* Delete shaders for cleanup */
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShader1);
+    glDeleteShader(fragmentShader2);
   };
 
   const auto render_cb = [&]() noexcept {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glUseProgram(shaderProgram1);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+    glUseProgram(shaderProgram2);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT,
+                   reinterpret_cast<void *>(3 * sizeof(unsigned int)));
   };
 
   const auto cleanup_cb = [&]() noexcept {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
+    glDeleteProgram(shaderProgram1);
+    glDeleteProgram(shaderProgram2);
   };
 
   try {
